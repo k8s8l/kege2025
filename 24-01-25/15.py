05ae53281158fa38e2e0878dd6d8c013c73ad308
User <korsik704@gmail.com>
2025-01-27 21:28:42 +0200

    test

---
+++ 24-01-25/15.py
@@ -0,0 +1,12 @@
+def f(a):
+    for x in range(1,11111):
+        f=((a,x) <=(x == a)or(x==1))
+        if not f:
+            return False
+    return True
+for a in range(1,11111):
+    if f(a):
+        print(a)
+        break
+
+
