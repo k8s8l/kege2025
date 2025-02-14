5026c531f6297ff284cbf8142da4890050b49fb6
User <korsik704@gmail.com>
2025-02-14 19:03:17 +0200

    dz

---
+++ ege-samostoyalka-14-02/n5.py
@@ -0,0 +1,13 @@
+ans=[]
+for n in range(0,10000):
+    r=bin(n)[2:]
+    if len(r)%3==0:
+        r=r+r[-3:]
+    else:
+        r=r + bin(n%3*3)[2:0]
+        r=int(r,10)
+        if r>170:
+            ans.append(r)
+print(max(ans))
+
+
