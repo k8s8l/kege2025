409c45e01fd3cf0d156872c47c22335357a17cb9
User <korsik704@gmail.com>
2025-02-21 18:17:18 +0200

    dz

---
+++ 21-02-25/н2.py
@@ -0,0 +1,8 @@
+for w in range(2):
+    for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                f = ((x<=z) <= w) or (not y)
+                if not f:
+                    print(w, x, y, z)
+#otvet yxzw
