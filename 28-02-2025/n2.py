983deec1903e155bd975ba8d48b1dfb58f8356c0
User <korsik704@gmail.com>
2025-02-28 18:08:28 +0200

    dz

---
+++ 28-02-2025/n2.py
@@ -0,0 +1,8 @@
+for h in range(2):
+    for l in range(2):
+        for w in range(2):
+            for n in range(2):
+                f = not(h<=l)<= (not(w<=n)) and h
+                if f:
+                    print(h, l, w, n)
+#otvet yxzw
