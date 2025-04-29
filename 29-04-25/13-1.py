3f972b6ff436b7221bc7f92bb8f29f636548e686
DKiselev <korsik704@gmail.com>
2025-04-29 20:01:02 +0200

    NUMB2

---
+++ 29-04-25/13-1.py
@@ -0,0 +1,9 @@
+from ipaddress import ip_network,ip_address
+for mask in range(16,25):
+    net=ip_network(f'238.51.1.202/{mask}',False)
+    cnt=0
+    if:
+        all(f'{int(238.51.1.202):032b}'[:16].count('1')>=f'{int(238.51.1.202):032b}'[16:].count('1')
+for ip in net):
+
+print()
